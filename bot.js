45b5a7284ba2c9df05f9d2e2a0028b348617dc81
KAERGSTY <137520623+KAERGSTY@users.noreply.github.com>
2026-04-09 17:52:39 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,18 @@
-const { Telegraf } = require('telegraf');
+const { Telegraf, Markup } = require('telegraf');
 
+// Pega aquí el Token que te dio @BotFather
 const bot = new Telegraf('7650482106:AAHV0-Y1yCE-jxyYlVVoIyo4iRUlZrB8zdg');
 
-bot.start((ctx) => ctx.reply('¡Bot conectado y funcionando!'));
-bot.help((ctx) => ctx.reply('¿En qué puedo ayudarte?'));
-
-// Ejemplo de respuesta a un mensaje
-bot.on('text', (ctx) => {
-    ctx.reply(`Has dicho: ${ctx.message.text}`);
+bot.command('start', (ctx) => {
+    ctx.reply('¡Bienvenido al Kaergsty Hub!', 
+        Markup.keyboard([
+            // Aquí pones el link que conseguiste de GitHub Pages
+            [Markup.button.webApp('🚀 Abrir Catálogo', 'https://kaergsty.github.io/ATC/')]
+        ]).resize()
+    );
 });
 
+bot.help((ctx) => ctx.reply('¿En qué puedo ayudarte?'));
+
 bot.launch();
-console.log("Bot en marcha...");
+console.log("Bot en marcha y Mini App enlazada...");
