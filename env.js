6c316d0c758c7ef087c0d039f056be7047d294eb
KAERGSTY <137520623+KAERGSTY@users.noreply.github.com>
2026-04-19 16:10:29 -0500

    a

---
+++ env.js
@@ -0,0 +1,4 @@
+const ENV = {
+    SUPABASE_URL: "https://urmnngtfoavnmvbwqepq.supabase.co",
+    SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVybW5uZ3Rmb2F2bm12YndxZXBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MTE4NzcsImV4cCI6MjA5MTI4Nzg3N30.HnfoffLftMYWt2ZEkv1YEbG0vqRPWjB5IeQunj2I5cs"
+};
